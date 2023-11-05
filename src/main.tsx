0fe3fff5a7f1864b85ea81125b2a1cb12f677065
LWangllix <vanglikas.lukas@gmail.com>
2023-11-05 19:22:09 +0200

    setup sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import * as Sentry from "@sentry/react";
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { GlobalStyle, theme } from './utils/theme.ts';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
 import redux from './state/store';
+import { GlobalStyle, theme } from './utils/theme';
 
 const queryClient = new QueryClient();
 
 const { store, persistor } = redux;
+const env = process?.env;
+
+
+
+if (env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    environment: env.REACT_APP_ENVIRONMENT,
+    dsn: env.REACT_APP_SENTRY_DSN,
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes
+        )
+      })
+    ],
+    tracesSampleRate: 1,
+    release: env.REACT_APP_VERSION,
+    tracePropagationTargets: [env.REACT_APP_MAPS_HOST!]
+  });
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
