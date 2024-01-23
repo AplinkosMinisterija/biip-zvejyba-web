f718434c00dd929cd5e5d54fabe7e11362041a79
Dovile <dovile.maminskaite@am.lt>
2024-01-23 13:57:41 +0100

    Unused variable, import, function or class

--- src/components/other/DeleteComponent.tsx
+++ src/components/other/DeleteComponent.tsx
@@ -88,14 +88,6 @@ const DeleteButtonContainer = styled.div`
   }
 `;
 
-const DeleteButton = styled(Button)`
-  button {
-    border-color: ${({ theme }) => theme.colors.danger};
-    color: ${({ theme }) => theme.colors.danger};
-  }
-  min-width: fit-content;
-`;
-
 const StyledIcon = styled(Icon)`
   cursor: pointer;
   font-size: 1.8rem;
