3555632b9632cdf7e92bab0f0d8d92f7af4e3c79
Dovile <dovile.maminskaite@am.lt>
2024-05-30 20:09:09 +0300

    unused imports

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -2,10 +2,7 @@ import styled from 'styled-components';
 import { device, useGetCurrentRoute, useWindowSize } from '../../utils';
 import Div100vh from 'react-div-100vh';
 import SideBar from './SideBar';
-import BackHeader from '../headers/BackHeader';
-import LogoHeader from '../headers/LogoHeader';
 import ScrollableContentLayout from './ScrollableContentLayout';
-import ContentLayout from './ContentLayout';
 
 const DefaultLayout = ({ children, onScroll = () => {} }: any) => {
   const isMobile = useWindowSize(device.mobileL);
